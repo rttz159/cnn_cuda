001163f303744fdbb25ee83ea05c7998367b68f2
rttz159 <101782892+rttz159@users.noreply.github.com>
2025-08-19 11:34:00 +0800

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -10,7 +10,7 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 
-echo "=== Installing CUDA Toolkit 12.4 ==="
+echo "=== Installing CUDA Toolkit 12.9 ==="
 sudo apt-get -y install cuda-toolkit-12-9
 
 echo "=== Updating environment variables for CUDA 12.9 ==="
