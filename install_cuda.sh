44bba3d48c167cdbb4fdc3060167f8ea08adab87
rttz159 <101782892+rttz159@users.noreply.github.com>
2025-08-19 11:17:36 +0800

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -13,13 +13,13 @@ rm cuda-keyring_1.1-1_all.deb
 echo "=== Updating package list ==="
 sudo apt-get update
 
-echo "=== Installing CUDA Toolkit 13.0 ==="
-sudo apt-get -y install cuda-toolkit-13-0
+echo "=== Installing CUDA Toolkit 12.4 ==="
+sudo apt-get install -y cuda-toolkit-12-4
 
 echo "=== Updating environment variables ==="
-if ! grep -q "cuda-13.0/bin" ~/.bashrc; then
-    echo 'export PATH=/usr/local/cuda-13.0/bin:$PATH' >> ~/.bashrc
-    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+if ! grep -q "cuda-12.4/bin" ~/.bashrc; then
+    echo 'export PATH=/usr/local/cuda-12.4/bin:$PATH' >> ~/.bashrc
+    echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.4/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 fi
 
 echo "=== Reloading ~/.bashrc ==="
